c0821f10a388db0d5d79b6847de012b74b0600b1
Michael Dokolin <mikhail.dokolin@elastic.co>
2020-10-28 15:13:23 +0100

    Move screenshotting library

--- x-pack/plugins/screenshotting/server/screenshots/open_url.ts
+++ x-pack/plugins/screenshotting/server/screenshots/open_url.ts
@@ -6,46 +6,37 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import { LevelLogger, startTrace } from '../';
-import { LocatorParams, UrlOrUrlLocatorTuple } from '../../../common/types';
-import { HeadlessChromiumDriver } from '../../browsers';
-import { ConditionalHeaders } from '../../export_types/common';
+import apm from 'elastic-apm-node';
+import type { Logger } from 'src/core/server';
+import type { HeadlessChromiumDriver } from '../browsers';
+import type { ConditionalHeaders } from '../browsers';
 import { DEFAULT_PAGELOAD_SELECTOR } from './constants';
 
 export const openUrl = async (
-  timeout: number,
   browser: HeadlessChromiumDriver,
+  logger: Logger,
+  timeout: number,
   index: number,
-  urlOrUrlLocatorTuple: UrlOrUrlLocatorTuple,
-  conditionalHeaders: ConditionalHeaders,
-  logger: LevelLogger
+  url: string,
+  conditionalHeaders: ConditionalHeaders
 ): Promise<void> => {
   // If we're moving to another page in the app, we'll want to wait for the app to tell us
   // it's loaded the next page.
   const page = index + 1;
   const waitForSelector = page > 1 ? `[data-shared-page="${page}"]` : DEFAULT_PAGELOAD_SELECTOR;
-
-  const endTrace = startTrace('open_url', 'wait');
-  let url: string;
-  let locator: undefined | LocatorParams;
-
-  if (typeof urlOrUrlLocatorTuple === 'string') {
-    url = urlOrUrlLocatorTuple;
-  } else {
-    [url, locator] = urlOrUrlLocatorTuple;
-  }
+  const span = apm.startSpan('open_url', 'wait');
 
   try {
-    await browser.open(url, { conditionalHeaders, waitForSelector, timeout, locator }, logger);
+    await browser.open(url, { conditionalHeaders, waitForSelector, timeout }, logger);
   } catch (err) {
     logger.error(err);
     throw new Error(
-      i18n.translate('xpack.reporting.screencapture.couldntLoadKibana', {
+      i18n.translate('xpack.screenshotting.screencapture.couldntLoadKibana', {
         defaultMessage: `An error occurred when trying to open the Kibana URL: {error}`,
         values: { error: err },
       })
     );
   }
 
-  endTrace();
+  span?.end();
 };
